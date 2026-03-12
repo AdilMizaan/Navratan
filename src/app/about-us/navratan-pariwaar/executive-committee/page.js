45f32900c8ce29515fa1c02c10491d861ef20f62
Adil Mizaan <adilmizaan09@gmail.com>
2026-03-12 17:58:56 +0530

    add founder & Patrons page

---
+++ src/app/about-us/navratan-pariwaar/executive-committee/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ExecutiveCommittee() {
+  return (
+    <div style={{ padding: "100px 8%", textAlign: "center" }}>
+      <h1>Executive Committee</h1>
+      <p>Our dedicated executive committee members.</p>
+    </div>
+  );
+}
